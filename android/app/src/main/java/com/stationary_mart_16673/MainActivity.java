460914266be4d223527dcf165f9b7099d26132ae
Crowdbotics <team@crowdbotics.com>
2020-05-06 16:35:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/stationary_mart_16673/MainActivity.java
@@ -0,0 +1,15 @@
+package com.stationary_mart_16673;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "stationary_mart_16673";
+    }
+}
