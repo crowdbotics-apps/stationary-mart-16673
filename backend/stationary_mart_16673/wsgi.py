a9ecb471ea9ab86162267222bee2b40709df71ae
Crowdbotics <team@crowdbotics.com>
2020-05-06 16:41:57 +0000

    Update models

--- backend/stationary_mart_16673/wsgi.py
+++ backend/stationary_mart_16673/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stationary_mart_16673.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stationary_mart_16673.settings")
 
 application = get_wsgi_application()
